90412e698f876c26432c804ecf034e31242bc74f
PeterMwitiDev <mwitipet@gmal.com>
2024-10-11 01:18:19 +0300

    Finished first part of backend

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -136,7 +136,11 @@ export const refreshToken = async (req,res) => {
     }
 }
 
-//todo:get profile
-// export const getProfile = async (req, res) => {
 
-// }
+export const getProfile = async (req, res) => {
+    try{
+        res.json(req.user)
+    } catch(error){
+        res.status(500).json({ message: "Server error", error:error.message })
+    }
+}
